4a026743ca4ae514300d066b1a83372a1bbc8244
Semyon Dipner <semyondipner>
2024-09-12 00:06:02 +0300

    Return ubicorn

--- backend/api.py
+++ backend/api.py
@@ -51,4 +51,5 @@ async def healthcheck():
     raise HTTPException(status_code=503, detail="Service unhealthy")
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("api:app", host="127.0.0.1", port=8000,
+                reload=True)
