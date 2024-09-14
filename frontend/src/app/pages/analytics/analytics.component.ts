ed72e2253935ae267c277afca4f5f2facd3d47a6
Эмиль Деникаев <emildenikaev@MacBook-Air-Emil.local>
2024-09-14 04:30:47 +0400

    feat: predict

--- frontend/src/app/pages/analytics/analytics.component.ts
+++ frontend/src/app/pages/analytics/analytics.component.ts
@@ -1,14 +1,9 @@
-import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-
-import { Observable, catchError, map, of, startWith, takeUntil } from 'rxjs';
-   
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { Observable, catchError, of } from 'rxjs';
 import { Destroyer } from '../../base/destroyer';
-
 import {
   ApexAxisChartSeries,
   ApexChart,
-  ChartComponent,
   ApexDataLabels,
   ApexPlotOptions,
   ApexYAxis,
@@ -18,7 +13,23 @@ import {
   ApexGrid
 } from "ng-apexcharts";
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { AnalyticsService, DayPrediction, Prediction, PredictionsResponse } from './services/analytics.service';
+import { AnalyticsService, PredictionsResponse, PredResponse } from './services/analytics.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+
+
+export interface HistoryEntry {
+  date: string;
+  cnt: number;
+}
+
+export interface PredictionEntry {
+  date: string;
+  low: number;
+  median: number;
+  high: number;
+}
 
 export type ChartOptions = {
   series: ApexAxisChartSeries;
@@ -38,141 +49,236 @@ export type ChartOptions = {
   templateUrl: './analytics.component.html',
   styleUrls: ['./analytics.component.less']
 })
+export class AnalyticsComponent extends Destroyer {
+  public isLoading = false;
+  public filters!: FormGroup;
+  public items$!: Observable<{ item_id: string }[]>;
+  chartOptions: Partial<ChartOptions> | any = {};
+  isAnalyticsExist = false;
 
-export class AnalyticsComponent extends Destroyer implements OnInit{
-  public isLoading = true;
-  chartData: any = []
+  private historyData: HistoryEntry[] = []; // Хранит данные от getHistory
+  private predictionsData: PredictionEntry[] = []; // Хранит данные от getPredictions
+  public displayedColumnsPredictions: string[] = ['date', 'low', 'median', 'high'];
+  public dataTablePredictions = new MatTableDataSource<any>([]);
 
-  chartOptions: Partial<ChartOptions> | any= {};
-
+  @ViewChild('paginatorPredictions') paginatorPredictions!: MatPaginator;
+  
   constructor(
     private _cdr: ChangeDetectorRef,
     private _snackBar: MatSnackBar,
     private _analyticsService: AnalyticsService
   ) {
-    super()
+    super();
+    this.items$ = this._analyticsService.getItems();
+    this.filters = new FormGroup({
+      item_id: new FormControl([], Validators.required),
+    });
   }
 
-  ngOnInit(): void {
-    this.getPredictions()
+  createReport() {
+    const item_id = this.filters.get('item_id')?.value;
+
+    if (item_id) {
+      this.getHistory(item_id);
+      this.getPredictions(item_id);
+    } else {
+      this.openSnackBar("Данные выбраны неверно", "Закрыть");
+    }
   }
 
-  getPredictions() {
+  getHistory(item_id: string) {
     this.isLoading = true;
     this._analyticsService
-        .getPredictions()
-        .pipe(
-            catchError((err: any) => {
-                const errorMessage = err.error && err.error.detail ? err.error.detail : "Произошла ошибка при загрузке файла.";
-                this.openSnackBar(errorMessage, "Закрыть");
-                return of(null);
-            })
-        )
-        .subscribe((response: PredictionsResponse | null) => {
-            if (response) {
-                this.chartData = response.predictions;
+      .getHistory(item_id)
+      .pipe(
+        catchError((err: any) => {
+          const errorMessage = err.error && err.error.detail ? err.error.detail : "Произошла ошибка при загрузке файла.";
+          this.openSnackBar(errorMessage, "Закрыть");
+          return of(null);
+        })
+      )
+      .subscribe((response: PredictionsResponse | null) => {
+        if (response && Array.isArray(response)) {
+          this.historyData = response; // Сохраняем данные истории
+          this.updateChartOptions();
+          this.isAnalyticsExist = true;
+        } else {
+          this.openSnackBar("Нет данных для отображения.", "Закрыть");
+        }
+        this.isLoading = false;
+        this._cdr.detectChanges();
+      });
+  }
 
-                this.updateChartOptions();
+  getPredictions(item_id: string) {
+    this.isLoading = true;
+    this._analyticsService
+      .getPredictions(item_id)
+      .pipe(
+        catchError((err: any) => {
 
-                this._cdr.detectChanges();
-                this.isLoading = false;
-            }
-        });
-}
-updateChartOptions(): void {
-  const dates: string[] = [];
-    const medianData: number[] = [];
+
+          const errorMessage = err.error && err.error.detail ? err.error.detail : "Произошла ошибка при загрузке файла.";
+          this.openSnackBar(errorMessage, "Закрыть");
+          return of(null);
+        })
+      )
+      .subscribe((response: PredResponse | null) => {
+        if (response && Array.isArray(response)) {
+          this.predictionsData = response; // Сохраняем данные прогноза
+          this.updateChartOptions();
+          this.populatePredictionsTable(); // Заполняем таблицу данными
+  
+          this.isAnalyticsExist = true;
+        } else {
+          this.openSnackBar("Нет данных для отображения.", "Закрыть");
+        }
+        this.isLoading = false;
+        this._cdr.detectChanges();
+      });
+  }
+
+  populatePredictionsTable() {
+    const formattedData = this.predictionsData.map((prediction: PredictionEntry) => ({
+      date: this.formatDate(prediction.date),
+      low: prediction.low,
+      median: prediction.median,
+      high: prediction.high,
+    }));
+  
+  
+  this.dataTablePredictions.data = formattedData; 
+  
+  setTimeout(() => {this.dataTablePredictions.paginator = this.paginatorPredictions
+      this._cdr.detectChanges();
+  })
+  }
+
+  applyFilter(event: Event, dataSource: MatTableDataSource<any>) {
+    const filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
+    dataSource.filter = filterValue;
+  
+    if (dataSource.paginator) {
+      dataSource.paginator.firstPage();
+    }
+  }
+  
+
+  updateChartOptions(): void {
+    const dates: string[] = [];
+    const cntData: number[] = [];
     const lowData: number[] = [];
+    const medianData: number[] = [];
     const highData: number[] = [];
-    this.chartData.forEach((prediction: Prediction) => {
-        prediction.day_prediction.forEach((day: DayPrediction) => {
-            dates.push(this.formatDate(day.date));
-            medianData.push(day.median);
-            lowData.push(day.low);
-            highData.push(day.high);
-        });
-    });
 
+    // Заполняем данные из getHistory
+    if (Array.isArray(this.historyData)) {
+      this.historyData.forEach((history: HistoryEntry) => {
+        dates.push(this.formatDate(history.date));
+        cntData.push(history.cnt);
+        // Заполняем данные из getPredictions по умолчанию как 0 или null
+        lowData.push(0);
+        medianData.push(0);
+        highData.push(0);
+      });
+    }
 
-  this.chartOptions = {
-      series: [
-          {
-              name: "Прогноз (медиана)",
-              data: medianData
-          },
-          {
-              name: "Нижняя граница",
-              data: lowData
-          },
-          {
-              name: "Верхняя граница",
-              data: highData
+    // Определяем lastDateFromHistory для дальнейшей обработки
+    const lastDateFromHistory = this.historyData.length > 0 ? new Date(this.historyData[this.historyData.length - 1].date) : null;
+
+    // Заполняем данные из getPredictions
+    if (Array.isArray(this.predictionsData)) {
+      this.predictionsData.forEach((prediction: PredictionEntry) => {
+        const predictionDate = new Date(prediction.date);
+        // Проверяем, если эта дата позже последней даты истории
+        if (lastDateFromHistory && predictionDate > lastDateFromHistory) {
+          // Добавляем только уникальные даты в массив dates
+          const formattedDate = this.formatDate(prediction.date);
+          if (!dates.includes(formattedDate)) {
+            dates.push(formattedDate);
+            cntData.push(0); // Заполняем значение для источника данных cnt как null
           }
+          // Заполняем данные low, median и high для этой даты
+          lowData.push(prediction.low);
+          medianData.push(prediction.median);
+          highData.push(prediction.high);
+        }
+      });
+    }
+
+    // Настройка параметров графика
+    this.chartOptions = {
+      series: [
+        {
+          name: "Продажи (cnt) ",
+          data: cntData
+        },
+        {
+          name: "Нижняя граница прогноза",
+          data: lowData
+        },
+        {
+          name: "Прогнозное значение (медиана)",
+          data: medianData
+        },
+        {
+          name: "Верхняя граница прогноза",
+          data: highData
+        }
       ],
       chart: {
-          height: 500,
-          type: "line",
-          zoom: {
-            type: "x",
-            enabled: true,
-            autoScaleYaxis: true
-          },
+        height: 500,
+        type: "line",
+        zoom: {
+          type: "x",
+          enabled: true,
+          autoScaleYaxis: true
+        },
       },
       dataLabels: {
-          enabled: false
+        enabled: false
       },
       stroke: {
-          width: [2, 2, 2],
-          curve: 'smooth'
+        width: [2, 2, 2, 2],
+        curve: 'smooth'
       },
-   
       grid: {
         row: {
           colors: ["#fff", "#f2f2f2"]
         }
       },
       xaxis: {
-          categories: dates,
-          tickPlacement: "on",
-          
-      },   
+        categories: dates,
+        tickPlacement: "on",
+      },
       yaxis: {
-          title: {
-              text: 'Продажи'
-          },
-          labels: {
-              formatter: (value: number) => value.toFixed(2)
-          }
-      },
-      tooltip: {
-          shared: true,
-          intersect: false,
-      },
-      fill: {
-        type: "gradient",
-        gradient: {
-          shade: "light",
-          type: "horizontal",
-          shadeIntensity: 0.25,
-          gradientToColors: undefined,
-          inverseColors: true,
-          opacityFrom: 0.85,
-          opacityTo: 0.85,
+        title: {
+          text: 'Значения'
+        },
+        labels: {
+          formatter: (value: number) => value !== null ? value.toFixed(2) : value
         }
       },
-      colors: ["#5F5F5F", "#FF4560", "#008FFB"] 
-  };
-  this.isLoading = false;
+      tooltip: {
+        shared: true,
+        intersect: false,
+      },
+      fill: {
+        type: "solid",
+      },
+      colors: ["#B0B0B0", "#7F7F7F", "#5F5F5F", "#B0B0B0"] // разные оттенки серого
+    };
 
-  this._cdr.detectChanges();
-}
+    // Обновляем график
 
 
-formatDate(dateString: string): string {
-  const date = new Date(dateString);
-  return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
-}
+    this._cdr.detectChanges();
+  }
 
+  formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+  }
 
   openSnackBar(message: string, action: string) {
     this._snackBar.open(message, action, {
@@ -182,5 +288,3 @@ formatDate(dateString: string): string {
     });
   }
 }
-
-
