5df3eb30407d30755ceb5fa32dadda7930d9123c
Semyon Dipner <semyondipner>
2024-09-16 11:42:52 +0300

    Format clustering,py

--- backend/services/clustering.py
+++ backend/services/clustering.py
@@ -1,18 +1,17 @@
-from models.clustering import Clustering, ClusteringItem
+""" Clustring Routes """
 from sqlalchemy import func, distinct
+from models.clustering import Clustering, ClusteringItem
 
 
 def get_dataframe(cluster, store_id, session):
+    """ Get DataFrame """
     query = (
         session.query(Clustering.store_id, Clustering.date, func.sum(Clustering.cnt).label('cnt'))
         .filter(Clustering.cluster == cluster, Clustering.store_id == store_id)
         .group_by(Clustering.store_id, Clustering.date)
     )
-
     results = query.all()
-
     session.close()
-
     items = []
     for result in results:
         item = ClusteringItem(
@@ -25,22 +24,22 @@ def get_dataframe(cluster, store_id, session):
 
 
 def get_clusters(session):
-
+    """ Get Clusters """
     query = session.query(distinct(Clustering.cluster))
-
     results = query.all()
     unique_clusters = [result[0] for result in results]
-
     session.close()
     return unique_clusters
 
 
 def get_clusters_items(cluster, store_id, session):
-    query = session.query(distinct(Clustering.item_id)).filter(Clustering.cluster == cluster, Clustering.store_id == store_id)
-
+    """ Get Clusters Items """
+    query = (
+        session
+        .query(distinct(Clustering.item_id))
+        .filter(Clustering.cluster == cluster, Clustering.store_id == store_id)
+    )
     results = query.all()
     unique_clusters = [result[0] for result in results]
-
     session.close()
     return unique_clusters
-
