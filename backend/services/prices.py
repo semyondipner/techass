8868788fc652c79b313b53c344099cbe49d5a47a
Semyon Dipner <semyondipner>
2024-09-16 11:45:30 +0300

    Format prices.py

--- backend/services/prices.py
+++ backend/services/prices.py
@@ -1,24 +1,34 @@
-from models.price import Price
-from sqlmodel import delete
+""" Prices """
+
+# Data Processing
 import pandas as pd
-from database.connection import engine_url
+
+# SQL Connections
 import psycopg2
 import sqlalchemy
+from sqlmodel import delete
+
+# Local Imports
+from models.price import Price
+from database.connection import engine_url
 
 
 def create_price(price: Price, session):
+    """ create_price """
     session.add(price)
     session.commit()
     session.refresh(price)
 
 
 def create_prices(file):
+    """ create_prices """
     with pd.read_csv(file, chunksize=10000) as reader:
         for chunk in reader:
                 chunk.to_sql("price", engine_url, if_exists='append', index=False)
 
 
 def delete_data(session):
+    """ delete_data """
     session.exec(delete(Price))
     session.commit()
     session.close()
